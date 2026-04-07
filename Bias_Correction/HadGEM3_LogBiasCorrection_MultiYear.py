a3c9536b9af092f8eaa327d9e3871359f716431d
Robert Potts <robert.potts@metoffice.gov.uk>
2026-04-07 18:40:07 +0100

    added comments

--- Bias_Correction/HadGEM3_LogBiasCorrection_MultiYear.py
+++ Bias_Correction/HadGEM3_LogBiasCorrection_MultiYear.py
@@ -30,7 +30,11 @@ run_type = os.environ.get("CYLC_TASK_PARAM_runtype", "hist")  # 'hist' or 'histn
 print(f'Processing Country: {Country}, baseline member: {baseline_member}, run type: {run_type}')
 
 folder = '/data/scratch/chantelle.burton/SoW2526/'
-YEAR = 2024
+DATA_YEAR = 2024 #this is the year that we're pulling out from the historical and historical-natural attribution ensembles. #TO GENERALISE to 2020-2024 once we have that data.
+TARGET_YEAR = 2024 # this is the year we want the regression to be relative to (i.e. the year we want to bias correct to). 
+BASELINE_START_YEAR = 1960
+BASELINE_END_YEAR = 2012
+
 #Set up the 2025 files and months automatically
 if Country == 'Korea':
     print('Running South Korea')
@@ -95,7 +99,7 @@ BiasCorrDict = {}
 # Step 0: Load fwi data from CSV using pandas
 df_obs = pd.read_csv(baseline_folder+'ERA5_FWI_1960-2013_'+Country+'_'+str(percentile)+'%.dat')  # Historical ERA5 array
 df_sim = pd.read_csv(baseline_folder+'HadGEM3_FWI_1960-2013_'+Country+'_'+str(baseline_member)+'_'+str(percentile)+'%.dat')  # Historical HadGEM array
-df_obs[np.isnan(df_obs)] = 0.000000000001 
+df_obs[np.isnan(df_obs)] = 0.000000000001 #nans break the soft log transform so fill val ~ 0 to avoid issues.
 df_sim[np.isnan(df_sim)] = 0.000000000001 
 
 #### Log transform the data here #### 
@@ -103,14 +107,14 @@ df_obs = np.log(np.exp(df_obs)-1)
 df_sim = np.log(np.exp(df_sim)-1)
 
 # Extract years and FWI values
-years = np.arange(1960, 2013)
+years = np.arange(BASELINE_START_YEAR, BASELINE_END_YEAR + 1) #arange uses the stop value as exclusive, so add 1 to include the end year.
 fwi_sim = df_sim.values
 fwi_sim = fwi_sim[:, 0]
 fwi_obs = df_obs.values
 fwi_obs = fwi_obs[:, 0]
 
 # Step 1a: Fit a linear regression model to obs and sim
-t = years - 2025 #2024#{set this as a new TARGET_YEAR parameter}  # shift years to be relative to 2025
+t = years - TARGET_YEAR #shift years relative to the target year.
 X = sm.add_constant(t)  # add a constant term for intercept
 
 def find_regression_parameters(fwi):
@@ -125,8 +129,7 @@ fwi0_obs, delta_obs, std_obs = find_regression_parameters(fwi_obs)
 
 
 
-# --- Refactored output: collect all data, write one CSV per baseline member/run type ---
-index_filestem = index_filestem1 if run_type == 'hist' else index_filestem2
+index_filestem = index_filestem1 if run_type == 'hist' else index_filestem2 #pull cylc run_type parameter in and do a hist or histnat run. 
 ensemble_members = np.arange(1, 106)  # 105 ensemble members
 realisations = np.arange(1, 6)        # 5 realisations per ensemble member
 n_years = len(years)
@@ -134,9 +137,9 @@ n_cols = len(ensemble_members) * len(realisations)
 data_matrix = np.full((n_years, n_cols), np.nan)
 col_names = []
 
-for e_idx, ensemble_member in enumerate(ensemble_members):
-    for r_idx, realisation in enumerate(realisations):
-        col_idx = e_idx * len(realisations) + r_idx
+for e_idx, ensemble_member in enumerate(ensemble_members): # loop through all 105 ensemble members
+    for r_idx, realisation in enumerate(realisations): #loop through physics realisations 1-5 for each of the 105 ensemble members
+        col_idx = e_idx * len(realisations) + r_idx #number of columns (should be 5 * 105)
         col_names.append(f"Ens{ensemble_member}_Real{realisation}")
         try:
             if ensemble_member < 10:
@@ -146,20 +149,20 @@ for e_idx, ensemble_member in enumerate(ensemble_members):
             else:
                 cube = iris.load_cube(folder+'Y2526FWI/FWI_HadGEM3-A-N216_r'+str(ensemble_member)+'i1p'+str(realisation)+'_'+index_filestem+'_20230601-20250201_global_day.nc', index_name)
             cube = contrain_to_sow_shapefile(cube, '/data/users/chantelle.burton/Attribution/StateOfFires_2025-26/SoW2526_Focal_MASTER_20260218.shp', shape_name)
-            cube = ConstrainToYear(cube, YEAR)
+            cube = ConstrainToYear(cube, DATA_YEAR)
             cube = CountryPercentile(cube, percentile)
             cube = TimePercentile(cube, percentile)
             data = np.ravel(cube.data)
 
-            # Log transform
+            # Soft Log transform
             data = np.log(np.exp(data)-1)
             # Detrend and scale
             data_corrected = fwi0_obs + (data - delta_sim * t - fwi0_sim)
-            # Inverse log transform
+            # Inverse soft log transform
             data_corrected = np.log(np.exp(data_corrected)+1)
 
             # Store in matrix
-            if len(data_corrected) == n_years:
+            if len(data_corrected) == n_years: #check that the length of the data matches the number of years (BASELINE_START_YEAR to BASELINE_END_YEAR)
                 data_matrix[:, col_idx] = data_corrected
             else:
                 print(f"Warning: Data length mismatch for Ens{ensemble_member} Real{realisation}")
